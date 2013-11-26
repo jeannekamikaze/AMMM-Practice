3c215ed678c52c0bf7fa93e4869c8468612aa211
vcrom <roger.hernando@gmail.com>
2013-11-26 23:41:23 +0100

    Bug fixed, due the matrices w_r and w_r have different # of rows and colums

--- meta/ParseDatFile.hpp
+++ meta/ParseDatFile.hpp
@@ -17,6 +17,14 @@ public:
 		vector<Mat> matrices = parseMatrices();
 		if(matrices.size() != 3) throw "we should have exacly 3 matrices in the .dat file";
 		delta = matrices[0];
+
+		cout <<"W_R size:" << matrices[1].size().height << endl;
+		cout <<"W_D size:" << matrices[2].size().width << endl;
+
+		Mat col = Mat::zeros(matrices[1].size().height, 1, CV_32FC1);
+		cv::hconcat(matrices[1], col, matrices[1]);
+		Mat row = Mat::zeros(1, matrices[2].size().width, CV_32FC1);
+		matrices[2].push_back(row);
 		merge(vector<Mat>(matrices.begin()+1,matrices.end()), w);	
 	}
 
